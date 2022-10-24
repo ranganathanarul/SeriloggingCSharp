41f2799873289285b9ed8f97eca52f4050b26a83
Ranganathan Palanisamy <ranganathanpalanisamy@hotmail.com>
2022-10-24 14:12:20 -0400

    API Framework Check-in

---
+++ SeriLoggerAPICore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeriLoggerAPICore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Get Method Called");
+            var rng = new Random();
+            var enumsValue = (IEnumerable<WeatherForecast>)null;
+            
+            try
+            {
+                enumsValue = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                   .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception occured on get function");
+            }
+            finally
+            {
+                _logger.LogInformation("Get method Completed.");
+            }
+            return enumsValue;
+        }
+    }
+}
